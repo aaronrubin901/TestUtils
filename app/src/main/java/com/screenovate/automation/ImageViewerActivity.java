02fa1eb6a10125ab5d18ba8cca454f33d6a117f7
Aharon <aharonr@ravtech.co.il>
2016-12-22 16:51:33 +0200

    added image viewer activity

--- app/src/main/java/com/screenovate/automation/ImageViewerActivity.java
+++ app/src/main/java/com/screenovate/automation/ImageViewerActivity.java
@@ -1,19 +1,7 @@
 package com.screenovate.automation;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class ImageViewerActivity extends AppCompatActivity {
     public static final String TAG = "IMAGEVIEWERLOG";
@@ -21,15 +9,8 @@ public class ImageViewerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        String imagePath = Environment.getExternalStorageDirectory().toString() + "checkerboard.bmp";
-        Log.i(TAG, imagePath);
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-        ImageView jpgView = (ImageView)findViewById(R.id.imageView);
-        jpgView.setImageBitmap(bitmap);
-
-            }
+        setContentView(R.layout.image_viewer);
+    }
 
         }
 
--- app/src/main/java/com/screenovate/automation/TestActivity.java
+++ app/src/main/java/com/screenovate/automation/TestActivity.java
@@ -1,19 +1,91 @@
 package com.screenovate.automation;
 
 import android.app.Activity;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.widget.Toast;
 
 
 public class TestActivity extends Activity {
 
-    private static final String TAG = TestActivity.class.getName();
+    private static final String TAG = "TestActivity";
+
+    KeyEvent event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "TEST ACTIVITY STARTTED");
+        setContentView(R.layout.testactivity);
         super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MENU:
+                Log.i(TAG, "Menu key released");
+                return true;
+            case KeyEvent.KEYCODE_SEARCH:
+                Log.i(TAG, "Search key released");
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (event.isTracking() && !event.isCanceled())
+                    Log.i(TAG, "Volumen Up released");
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                Log.i(TAG, "Volumen Down released");
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.i(TAG, "KEY EVENT REGIRSTER" + keyCode);
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MENU:
+                Log.i(TAG, "Menu key pressed");
+                return true;
+            case KeyEvent.KEYCODE_SEARCH:
+               Log.i(TAG, "Search key pressed");
+                return true;
+            case KeyEvent.KEYCODE_BACK:
+                onBackPressed();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                event.startTracking();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+               Log.i(TAG, "Volumen Down pressed");
+                return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+       Log.i(TAG, "Pressed for a long time =) ");
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+       Log.i(TAG, "Back key pressed =)");
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+       Log.i(TAG, "Touch press on x: " + x + " y: " + y);
+        return true;
     }
 
     @Override
