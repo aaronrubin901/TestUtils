0b2e0147c113c51edc4f7d4648ae5ab09b563e09
Aharon <aharonr@ravtech.co.il>
2016-12-27 11:57:56 +0200

    added receive sms service

---
+++ app/src/main/java/com/screenovate/automation/SmsUtilService.java
@@ -0,0 +1,67 @@
+package com.screenovate.automation;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class SmsUtilService extends Service {
+    private static final String TAG = "SmsUtil";
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            if (extras.containsKey("SentSms")) {
+                Log.i(TAG, "Logging Last Sent Sms" + extras.getString("SentSms"));
+                getLastSentSMS();
+            }
+
+            if (extras.containsKey("ReceiveSms")) {
+                Log.i(TAG, "Logging Last Receive Sms");
+                getLastReceiveSMS();
+
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void getLastReceiveSMS() {
+        Uri sentURI = Uri.parse("content://sms/inbox");
+        ContentResolver cr = getContentResolver();
+        Cursor c = cr.query(sentURI, null, null, null, "date desc limit 1");
+        String str = "";
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            str = "Message receive: from address " + c.getString(c.getColumnIndex("address")) + " body: " +
+                    c.getString(c.getColumnIndex("body"));
+        }
+        Log.i(TAG, str);
+
+    }
+
+    private void getLastSentSMS() {
+        Uri sentURI = Uri.parse("content://sms/sent");
+        ContentResolver cr = getContentResolver();
+        Cursor c = cr.query(sentURI, null, null, null, "date desc limit 1");
+        String str = "";
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            str = "Message sent: to address " + c.getString(c.getColumnIndex("address")) + " body: " +
+                    c.getString(c.getColumnIndex("body"));
+        }
+        Log.i(TAG, str);
+
+    }
+
+}
