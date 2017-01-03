bd0528e8a46a8ce77acdf2d68f0a1d30e9494a75
Aharon <aharonr@ravtech.co.il>
2017-01-03 14:48:15 +0200

    added input text box in test activity

--- app/src/main/java/com/screenovate/automation/CreateNotification.java
+++ app/src/main/java/com/screenovate/automation/CreateNotification.java
@@ -6,6 +6,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
+import android.nfc.Tag;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -14,19 +16,31 @@ import java.util.Random;
 
 
 public class CreateNotification extends Service {
-    public int getRandom(){
-        Random r = new Random();
-        int i1 = r.nextInt(10000 - 100) + 100;
-        return i1;
-    }
+    public static final String TAG = "AutomationNotification";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String randNumber = String.valueOf(getRandom());
-        Log.i("AutomationNotification", "Random notifiction number is " + randNumber);
+        String randomNumber = "";
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            if (extras.containsKey("random")) {
+                randomNumber = extras.getString("random");
+                Log.i(TAG, "Creating notification with random number " + randomNumber);
+                createNotification(randomNumber);
+            }
+            else {
+                Log.w(TAG, "Please insert Random number");
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void createNotification(String randomNumber) {
+        Log.i(TAG, "Random notification number is " + randomNumber);
         Notification noti = new Notification.Builder(this)
                 .setContentTitle("Automation Notification")
-                .setContentText(randNumber)
+                .setContentText(randomNumber)
                 .setSmallIcon(R.mipmap.ic_notify)
                 .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
                         R.mipmap.ic_notify))
@@ -34,9 +48,7 @@ public class CreateNotification extends Service {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         // hide the notification after its selected
         noti.flags |= Notification.FLAG_AUTO_CANCEL;
-
         notificationManager.notify(0, noti);
-        return super.onStartCommand(intent, flags, startId);
     }
 
     @Nullable
